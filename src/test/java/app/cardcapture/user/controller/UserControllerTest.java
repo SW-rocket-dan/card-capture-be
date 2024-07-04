f87d66a8d540f389a15ac480f076576aa9881ae5
inpink <inpink@kakao.com>
2024-07-04 15:54:36 +0900

    test: 프론트엔드에서 구글 개인정보를 조회할 수 있다.
    GET localhost:8080/api/v1/user/me

---
+++ src/test/java/app/cardcapture/user/controller/UserControllerTest.java
@@ -0,0 +1,25 @@
+package app.cardcapture.user.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void 구글_유저_정보를_조회할_수_있다() throws Exception {
+        mockMvc.perform(get("/api/v1/user/me"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").exists());
+    }
+}
