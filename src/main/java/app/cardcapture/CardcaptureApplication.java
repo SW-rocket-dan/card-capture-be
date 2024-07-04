af258c65ce86a3f610ef6d9b4f2547d933231868
inpink <inpink@kakao.com>
2024-07-04 15:44:34 +0900

    chore: 초기 설정

---
+++ src/main/java/app/cardcapture/CardcaptureApplication.java
@@ -0,0 +1,13 @@
+package app.cardcapture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardcaptureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardcaptureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/cardcapture/CardcaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package app.cardcapture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardcaptureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
