f995b4456418efc8ca5f58b953956527964267e8
inpink <inpink@kakao.com>
2024-07-05 21:06:55 +0900

    refactor: ConfigurationProperties 적용

--- src/main/java/app/cardcapture/auth/google/config/GoogleAuthConfig.java
+++ src/main/java/app/cardcapture/auth/google/config/GoogleAuthConfig.java
@@ -1,34 +1,21 @@
 package app.cardcapture.auth.google.config;
 
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "oauth.google")
 @Getter
+@Setter
 public class GoogleAuthConfig {
-
-    @Value("${oauth.google.client-id}")
     private String clientId;
-
-    @Value("${oauth.google.redirect-uri}")
     private String redirectUri;
-
-    @Value("${oauth.google.response-type}")
     private String responseType;
-
-    @Value("${oauth.google.scope}")
     private String scope;
-
-    @Value("${oauth.google.client-secret}")
     private String clientSecret;
-
-    @Value("${oauth.google.base-url}")
     private String baseUrl;
-
-    @Value("${oauth.google.oauth-url}")
     private String oauthUrl;
-
-    @Value("${oauth.google.api-url}")
     private String apiUrl;
 }
