86b9e984655af3de0e1e2351d601bdc172342436
inpink <inpink@kakao.com>
2024-07-06 16:50:54 +0900

    feat: Spring Security 적용
    - Spring Security 6.3.0
    - 일단 지금은 모든 페이지에 대해 인가없이 접근 가능
    - 세션 사용 X
    - CSRF 필터 사용 X

---
+++ src/main/java/app/cardcapture/security/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package app.cardcapture.security.config;
+
+import app.cardcapture.security.filter.JwtAuthenticationTokenFilter;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@Getter
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        (requests) -> requests.anyRequest().permitAll()
+                );
+        http.sessionManagement(
+                        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
