6548299f71c41570518c1990e4591d3d48056d9f
inpink <inpink@kakao.com>
2024-07-12 15:11:07 +0900

    feat: 개발 환경에서, React 웹 브라우저에서 spring api 호출 가능하도록 CORS 허용

---
+++ src/main/java/app/cardcapture/common/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package app.cardcapture.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
