b0a6d36b4443bfcc09462365a222ea9429abbe81
inpink <inpink@kakao.com>
2024-07-12 15:56:28 +0900

    test: S3 프리사인 URL을 통한 이미지 저장 및 삭제 controller test
    @WebMvcTest는 Security 관련 빈도 뜬다. 이 테스트에서는 jwt filter를 끄고 테스트했다.
    추후 Controller Test는 Endpoint to Endpoint 통합 테스트로 변경할 수 있다.
    CORS test를 위해 CorsConfig를 import한다.

--- src/main/java/app/cardcapture/common/config/CorsConfig.java
+++ src/main/java/app/cardcapture/common/config/CorsConfig.java
@@ -1,20 +1,35 @@
 package app.cardcapture.common.config;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Getter
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.origins}")
+    private String corsOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+        String[] origins = getSplitedOrigins();
+
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:3000")
+                .allowedOrigins(origins)
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    public String[] getSplitedOrigins() {
+        String[] origins = StringUtils.tokenizeToStringArray(corsOrigins, ",");
+        return origins;
     }
 }
