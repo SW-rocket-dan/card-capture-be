47aaba4f2622549ff78fbbe0b70d8ac613b9d75b
inpink <inpink@kakao.com>
2024-07-17 00:15:05 +0900

    feat: 공통 응답 포맷을 Controller에 적용한다

--- src/main/java/app/cardcapture/sticker/controller/StickerController.java
+++ src/main/java/app/cardcapture/sticker/controller/StickerController.java
@@ -1,5 +1,6 @@
 package app.cardcapture.sticker.controller;
 
+import app.cardcapture.common.dto.ResponseDto;
 import app.cardcapture.sticker.dto.StickerResponseDto;
 import app.cardcapture.sticker.dto.StickerSaveRequestDto;
 import app.cardcapture.sticker.service.StickerService;
@@ -17,7 +18,7 @@ import java.util.List;
 @RequestMapping("/sticker")
 @RequiredArgsConstructor
 public class StickerController {
-
+    private static final String STICKER_SEARCH_SUCCESS = "Stickers retrieved successfully";
     private final StickerService stickerService;
 
     @Hidden
@@ -32,13 +33,18 @@ public class StickerController {
     }
 
     @Operation(summary = "검색어를 통해 스티커 조회",
-            description = "검색어로 스티커를 조회합니다. 한글 또는 영어 태그 중 하나라도 일치하면 해당 스티커를 반환합니다.")
+            description = "검색어로 스티커를 조회합니다. 한글 또는 영어 태그 중 하나라도 검색어가 포함되면 해당 스티커를 반환합니다. 영어는 대소문자 상관없이 검색됩니다. " +
+                    "검색 가능한 태그는 다음과 같습니다.(영문) 이는 예시이며, 스웨거에 없는 다른 것들도 스티커가 있다면 검색될 수 있습니다." +
+                    "Apple, Banana, Cherry, Date, Elderberry, Fig, Grape, Honeydew, Ivy Gourd, Jackfruit, Kiwi, Lemon, Mango, Nectarine, Orange, Papaya, Quince, Raspberry, Strawberry, Tomato, Ugli Fruit," +
+                    "Cat, Dog, Flower, Heart, Star, Cake, Balloon, Gift, Rainbow, Sunshine, Moon, Cloud, Car, Airplane, Boat, Train, Mountain, Tree, Beach, Sea, Book, Instrument, Music, Movie, Sports, Travel, Camera, Smile, Happiness, Friendship, Love, Fashion, Art, Food, Coffee, Tea, Dessert, Celebration, Party, Birthday, Wedding, Shoes, Clothing, Accessory, Family, Nature, Animal, Plant"
+    )
     @GetMapping("/search")
-    public ResponseEntity<List<StickerResponseDto>> searchStickers(
+    public ResponseEntity<ResponseDto<List<StickerResponseDto>>> searchStickers(
             @RequestParam String searchTerm
     ) {
         List<StickerResponseDto> stickers = stickerService.searchStickers(searchTerm);
-        return ResponseEntity.ok(stickers);
-    }
+        ResponseDto<List<StickerResponseDto>> response = ResponseDto.createSuccess(STICKER_SEARCH_SUCCESS, stickers);
 
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/app/cardcapture/user/controller/UserController.java
+++ src/main/java/app/cardcapture/user/controller/UserController.java
@@ -1,12 +1,11 @@
 package app.cardcapture.user.controller;
 
 import app.cardcapture.auth.jwt.dto.JwtAuthorizationDto;
-import app.cardcapture.common.dto.ErrorResponse;
+import app.cardcapture.common.dto.ResponseDto;
 import app.cardcapture.user.dto.UserDto;
 import app.cardcapture.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -23,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @AllArgsConstructor
 public class UserController {
 
+    private static final String USER_INFO_RETRIEVAL_SUCCESS = "User details retrieved successfully";
     private final UserService userService;
 
     @Operation(summary = "사용자 정보 조회",
@@ -30,19 +30,17 @@ public class UserController {
                     "JWT를 통해 사용자를 식별합니다. " +
                     "JWT가 유효하지 않으면 403을 반환합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "사용자 정보 조회 성공",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = UserDto.class))),
-            @ApiResponse(responseCode = "403", description = "JWT가 유효하지 않음",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ErrorResponse.class),
-                            examples = @ExampleObject(value = "{\"message\": \"JWT가 유효하지 않음\", \"code\": 403}")))})
+            @ApiResponse(responseCode = "200", description = "사용자 정보 조회 성공"),
+            @ApiResponse(responseCode = "403", description = "JWT가 유효하지 않음")})
     @GetMapping("/me")
-    public ResponseEntity<UserDto> getUserDetails(@RequestHeader(value="Authorization") @Valid JwtAuthorizationDto jwtAuthorizationDto) {
-        System.out.println("jwtAuthorizationDto = " + jwtAuthorizationDto);
+    public ResponseEntity<ResponseDto<UserDto>> getUserDetails(
+            @RequestHeader(value = "Authorization")
+            @Valid JwtAuthorizationDto jwtAuthorizationDto
+    ) {
         String accessToken = jwtAuthorizationDto.getAceessToken();
         UserDto userDto = userService.findUserByAccessToken(accessToken);
+        ResponseDto<UserDto> response = ResponseDto.createSuccess(USER_INFO_RETRIEVAL_SUCCESS, userDto);
 
-        return ResponseEntity.ok(userDto);
+        return ResponseEntity.ok(response);
     }
 }
