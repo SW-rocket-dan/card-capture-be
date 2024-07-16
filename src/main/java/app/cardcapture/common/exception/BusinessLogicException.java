b0e94ca0c715e225e8fedb476ab5c06cbe49e7ec
inpink <inpink@kakao.com>
2024-07-17 00:07:43 +0900

    feat: API 호출 응답 시 성공, 실패 공통 포맷

--- src/main/java/app/cardcapture/common/exception/BusinessLogicException.java
+++ src/main/java/app/cardcapture/common/exception/BusinessLogicException.java
@@ -3,7 +3,14 @@ package app.cardcapture.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class BusinessLogicException extends RuntimeException {
+    private final HttpStatus status;
+
     public BusinessLogicException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
