df8e5674241e68e03210b485eed9bbd166a0b6bb
inpink <inpink@kakao.com>
2024-07-31 23:14:35 +0900

    feat: Exception Handler에서 Response에 에러 로그를 담지 않는다
    사용자에게 서버의 로그를 보여주는 것은 보안상 문제가 있을 수 있다. 응답으로 에러를 보내는 대신 서버에 로그를 출력한다

--- src/main/java/app/cardcapture/common/exception/GlobalExceptionHandler.java
+++ src/main/java/app/cardcapture/common/exception/GlobalExceptionHandler.java
@@ -1,23 +1,26 @@
 package app.cardcapture.common.exception;
 
 import app.cardcapture.common.dto.ErrorResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessLogicException.class)
     public ResponseEntity<ErrorResponseDto<String>> handleBusinessLogicException(BusinessLogicException ex) {
-        ErrorResponseDto<String> response = ErrorResponseDto.create(ex.getMessage(), null);
+        ErrorResponseDto<String> response = ErrorResponseDto.create("알 수 없는 에러", null);
         return new ResponseEntity<>(response, ex.getStatus());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto<String>> handleGeneralException(Exception ex) {
-        ErrorResponseDto<String> response = ErrorResponseDto.create("Internal Server Error" + ex.getMessage(), null);
+        ErrorResponseDto<String> response = ErrorResponseDto.create("Internal Server Error", null);
+        log.error("Internal Server Error", ex);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
