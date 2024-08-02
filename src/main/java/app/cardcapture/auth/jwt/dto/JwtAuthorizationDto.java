ee58a4e22c3499af0c3adb6dcd331a87e0b6bd19
inpink <inpink@kakao.com>
2024-08-03 00:56:38 +0900

    refactor: change DTO classes to use records
    Converted DTO classes to Java records to simplify the code and leverage the benefits of immutable data structures.

--- src/main/java/app/cardcapture/auth/jwt/dto/JwtAuthorizationDto.java
+++ src/main/java/app/cardcapture/auth/jwt/dto/JwtAuthorizationDto.java
@@ -1,12 +1,8 @@
 package app.cardcapture.auth.jwt.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class JwtAuthorizationDto {
-    @NotBlank
-    private String aceessToken;
+public record JwtAuthorizationDto(
+        @NotBlank String aceessToken
+) {
 }
