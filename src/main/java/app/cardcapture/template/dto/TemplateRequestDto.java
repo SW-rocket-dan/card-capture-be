1840ddc73323c48c81403b377a08babc41fecbda
inpink <inpink@kakao.com>
2024-08-07 16:30:31 +0900

    refactor: make certain values optional in DTO when creating initial template
    Modified the DTO to make certain values non-mandatory during the initial creation of a template. This ensures flexibility in template creation and simplifies the initial setup process.

--- src/main/java/app/cardcapture/template/dto/TemplateRequestDto.java
+++ src/main/java/app/cardcapture/template/dto/TemplateRequestDto.java
@@ -3,16 +3,15 @@ package app.cardcapture.template.dto;
 import app.cardcapture.template.domain.entity.Prompt;
 import app.cardcapture.template.domain.entity.Template;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.NonNull;
 
 import java.util.List;
 
 public record TemplateRequestDto(
-        @NotBlank String title,
+        String title,
         String description,
-        @NotBlank String fileUrl,
-        @NonNull @Valid List<TemplateTagRequestDto> templateTagRequestDtos,
+        String fileUrl,
+        List<TemplateTagRequestDto> templateTagRequestDtos,
         @NonNull @Valid PromptRequestDto promptRequestDto
 
 ) {
--- src/main/java/app/cardcapture/template/dto/TemplateTagRequestDto.java
+++ src/main/java/app/cardcapture/template/dto/TemplateTagRequestDto.java
@@ -1,11 +1,10 @@
 package app.cardcapture.template.dto;
 
 import app.cardcapture.template.domain.entity.TemplateTag;
-import jakarta.validation.constraints.NotBlank;
 
 public record TemplateTagRequestDto(
-    @NotBlank String english,
-    @NotBlank String korean
+    String english,
+    String korean
 ) {
     public TemplateTag toEntity() {
         TemplateTag templateTag = new TemplateTag();
