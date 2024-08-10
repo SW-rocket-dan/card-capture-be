62d7caf68f06cbe889b62f2f4fefc6ab4fc36347
inpink <inpink@kakao.com>
2024-08-11 03:00:40 +0900

    feat: extract image from imageURL and store in S3

--- src/main/java/app/cardcapture/s3/service/S3Service.java
+++ src/main/java/app/cardcapture/s3/service/S3Service.java
@@ -1,12 +1,23 @@
 package app.cardcapture.s3.service;
 
+import app.cardcapture.common.exception.BusinessLogicException;
+import app.cardcapture.user.domain.entity.User;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.util.IOUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.util.Date;
@@ -72,6 +83,46 @@ public class S3Service {
             return fileUrl.substring(bucketUrlPattern.length());
         } else {
             throw new IllegalArgumentException("Invalid S3 URL: " + fileUrl);
+        }
+    }
+
+    public String uploadImageFromUrl(String path, String imageUrl, String fileName, String extension, User user) {
+        URL url = convertToURL(imageUrl);
+
+        HttpURLConnection connection = getHttpURLConnection(imageUrl, url);
+
+        try (InputStream inputStream = connection.getInputStream()) {
+            byte[] imageBytes = IOUtils.toByteArray(inputStream);
+
+            try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageBytes)) {
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentLength(imageBytes.length);
+                metadata.setContentType(connection.getContentType());
+
+                amazonS3.putObject(bucket+path, fileName+"."+extension+user.getId(), byteArrayInputStream, metadata);
+                return String.format("https://%s.s3.%s.amazonaws.com/poster/%s",
+                        bucket, "ap-northeast-2", fileName+"."+extension+user.getId());
+            }
+        } catch (IOException e) {
+            throw new BusinessLogicException("Error while reading the image from the URL: " + imageUrl, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private HttpURLConnection getHttpURLConnection(String imageUrl, URL url) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            return connection;
+        } catch (IOException e) {
+            throw new BusinessLogicException("Error while connecting to the image URL: " + imageUrl, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private URL convertToURL(String imageUrl) {
+        try {
+            return new URL(imageUrl);
+        } catch (MalformedURLException e) {
+            throw new BusinessLogicException("Invalid image URL: " + imageUrl, HttpStatus.BAD_REQUEST);
         }
     }
 }
