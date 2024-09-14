ad76014fdbff285568db2b522dadc84ddf23a0d9
inpink <inpink@kakao.com>
2024-09-14 16:51:05 +0900

    feat: use pessimistic locking when retrieving user's specific product category

--- src/main/java/app/cardcapture/payment/business/repository/UserProductCategoryRepository.java
+++ src/main/java/app/cardcapture/payment/business/repository/UserProductCategoryRepository.java
@@ -3,7 +3,11 @@ package app.cardcapture.payment.business.repository;
 import app.cardcapture.payment.business.domain.ProductCategory;
 import app.cardcapture.payment.business.domain.entity.UserProductCategory;
 import app.cardcapture.user.domain.entity.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +19,7 @@ public interface UserProductCategoryRepository extends JpaRepository<UserProduct
 
     boolean existsByUserAndProductCategory(User user, ProductCategory productCategory);
 
-    Optional<UserProductCategory> findByUserAndProductCategory(User user, ProductCategory productCategory);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT upc FROM UserProductCategory upc WHERE upc.user = :user AND upc.productCategory = :productCategory")
+    Optional<UserProductCategory> findByUserAndProductCategoryWithLock(@Param("user") User user, @Param("productCategory") ProductCategory productCategory);
 }
