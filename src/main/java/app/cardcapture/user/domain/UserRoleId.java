678a8428d842ddf04af65e3c7d4874ed3464ea56
inpink <inpink@kakao.com>
2024-09-18 01:08:19 +0900

    refactor: replace UserRoleId constructor with Lombok annotations

--- src/main/java/app/cardcapture/user/domain/UserRoleId.java
+++ src/main/java/app/cardcapture/user/domain/UserRoleId.java
@@ -2,23 +2,24 @@ package app.cardcapture.user.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRoleId implements Serializable {
 
     private Long user;
     private Role role;
 
-    public UserRoleId() {}
-
-    public UserRoleId(Long user, Role role) {
-        this.user = user;
-        this.role = role;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         UserRoleId that = (UserRoleId) o;
         return Objects.equals(user, that.user) && role == that.role;
     }
