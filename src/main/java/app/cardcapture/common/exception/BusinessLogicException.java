5aec214f83cce802df1ca43a736989111d29fc2e
inpink <inpink@kakao.com>
2024-09-17 19:41:26 +0900

    fix: resolve issue where null was logged on exception by passing message to RuntimeException
    Fixed the issue where null was printed in logs during exceptions by passing the message to the parent class (RuntimeException) using super(), ensuring the message is properly set and logged.

--- src/main/java/app/cardcapture/common/exception/BusinessLogicException.java
+++ src/main/java/app/cardcapture/common/exception/BusinessLogicException.java
@@ -2,10 +2,13 @@ package app.cardcapture.common.exception;
 
 import app.cardcapture.common.dto.ErrorCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class BusinessLogicException extends RuntimeException {
     private final ErrorCode errorCode;
+
+    public BusinessLogicException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
 }
