4136afe6bd5fe92e2d393fce4e9351ee2265caf9
inpink <inpink@kakao.com>
2024-09-17 21:40:10 +0900

    refactor: remove foreign key constraint between UserRole and User

--- src/main/java/app/cardcapture/user/domain/entity/UserRole.java
+++ src/main/java/app/cardcapture/user/domain/entity/UserRole.java
@@ -3,11 +3,13 @@ package app.cardcapture.user.domain.entity;
 import app.cardcapture.user.domain.Role;
 import app.cardcapture.user.domain.UserRoleId;
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -36,7 +38,7 @@ public class UserRole {
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false,  foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private User user;
 
     @Id
