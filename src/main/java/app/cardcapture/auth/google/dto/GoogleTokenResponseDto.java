9123572717a26487a33c55468da966fe8b81bda6
inpink <inpink@kakao.com>
2024-09-20 22:52:05 +0900

    refactor: use record for GoogleTokenResponseDto

--- src/main/java/app/cardcapture/auth/google/dto/GoogleTokenResponseDto.java
+++ src/main/java/app/cardcapture/auth/google/dto/GoogleTokenResponseDto.java
@@ -2,18 +2,14 @@ package app.cardcapture.auth.google.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
 
-@Getter
-@AllArgsConstructor
-@ToString
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class GoogleTokenResponseDto {
-    private String accessToken;
-    private String refreshToken;
-    private String idToken;
-    private String tokenType;
-    private int expiresIn;
+public record GoogleTokenResponseDto(
+    String accessToken,
+    String refreshToken,
+    String idToken,
+    String tokenType,
+    int expiresIn
+) {
+
 }
